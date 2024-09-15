8adab4eb6a24513ded3cb6e554bf106827abc005
Александър Ангелов <aleksandarangelov04@pmgkk.com>
2024-09-15 13:44:57 +0300

    Created map FAKT

--- Game/Builds/Maps/MapBuild.cpp
+++ Game/Builds/Maps/MapBuild.cpp
@@ -153,7 +153,7 @@ Map AlphabeticBuild::buildFAKT()
 
     #include "ObsticlesBuilds/FAKTObsticles.txt"
 
-    const char deathSymbol = 'd';
+    const char deathSymbol = ' ';
 
     map.addItem(make_polymorphic<NullItem>(43, 24, 'A', false));
     map.addItem(make_polymorphic<NullItem>(44, 24, 'n', false));
@@ -164,7 +164,7 @@ Map AlphabeticBuild::buildFAKT()
     map.addItem(make_polymorphic<DeathItem>(44, 20, deathSymbol));
     map.addItem(make_polymorphic<DeathItem>(44, 21, deathSymbol));
 
-    map.addItem(make_polymorphic<NullItem>(42, 21, 'c', false));
+    map.addItem(make_polymorphic<NullItem>(42, 21, 'C', false));
 
     map.addItem(make_polymorphic<DeathItem>(47, 22, deathSymbol));
     map.addItem(make_polymorphic<DeathItem>(43, 23, deathSymbol));
@@ -197,10 +197,115 @@ Map AlphabeticBuild::buildFAKT()
     map.addItem(make_polymorphic<DeathItem>(39, 23, deathSymbol));
     map.addItem(make_polymorphic<DeathItem>(39, 22, deathSymbol));
     map.addItem(make_polymorphic<TimyItem>(39, 20, 'T', 255));
-    map.addItem(make_polymorphic<TrollItem>(34, 22, 'T', 42, 3));
-   // map.addItem(make_polymorphic<DeathItem>(33, 23, deathSymbol));
-    //map.addItem(make_polymorphic<DeathItem>(47, 24, deathSymbol));
-   // map.addItem(make_polymorphic<DeathItem>(47, 24, deathSymbol));
+    map.addItem(make_polymorphic<TrollItem>(35, 22, 'T', 42, 3));
+
+    map.addItem(make_polymorphic<NullItem>(2, 28, 'B', false));
+    map.addItem(make_polymorphic<NullItem>(3, 28, 'I', false));
+    map.addItem(make_polymorphic<NullItem>(4, 28, 'L', false));
+    map.addItem(make_polymorphic<NullItem>(5, 28, 'L', false));
+    map.addItem(make_polymorphic<NullItem>(6, 28, 'A', false));
+
+    map.addItem(make_polymorphic<DeathItem>(5, 27, deathSymbol));
+
+    map.addItem(make_polymorphic<Item>(4, 26, 'A', 44, 44));
+
+    map.addItem(make_polymorphic<DeathItem>(1, 23, deathSymbol));
+    map.addItem(make_polymorphic<TeleportItem>(4, 24, 2, 6));
+    map.addItem(make_polymorphic<TeleportItem>(3, 6, 24, 1));
+    map.addItem(make_polymorphic<Item>(1, 6, ' ', 20, 44));
+
+    map.addItem(make_polymorphic<TeleportItem>(35, 8, 40, 2));
+    map.addItem(make_polymorphic<TeleportItem>(40, 3, 24, 1));
+    map.addItem(make_polymorphic<Item>(41, 1, 'K', 32, 50));
+    map.addItem(make_polymorphic<DeathItem>(42, 1, deathSymbol));
+
+    map.addItem(make_polymorphic<NullItem>(10, 24, 'd', false));
+    map.addItem(make_polymorphic<NullItem>(11, 24, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(12, 24, 'n', false));
+    map.addItem(make_polymorphic<NullItem>(13, 24, 't', false));
+
+    map.addItem(make_polymorphic<NullItem>(10, 21, 'k', false));
+    map.addItem(make_polymorphic<NullItem>(11, 21, 'n', false));
+    map.addItem(make_polymorphic<NullItem>(12, 21, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(13, 21, 'w', false));
+
+    map.addItem(make_polymorphic<NullItem>(16, 23, 'y', false));
+    map.addItem(make_polymorphic<NullItem>(16, 22, 'e', false));
+    map.addItem(make_polymorphic<NullItem>(16, 21, 't', false));
+
+    map.addItem(make_polymorphic<DeathItem>(1, 18, deathSymbol));
+    map.addItem(make_polymorphic<DeathItem>(2, 18, deathSymbol));
+    map.addItem(make_polymorphic<NullItem>(1, 17, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(2, 17, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(3, 17, 'p', false));
+
+    map.addItem(make_polymorphic<NullItem>(1, 16, 't', false));
+    map.addItem(make_polymorphic<NullItem>(2, 16, 'e', false));
+    map.addItem(make_polymorphic<NullItem>(3, 16, 'o', false));
+    map.addItem(make_polymorphic<DeathItem>(4, 16, deathSymbol));
+    map.addItem(make_polymorphic<DeathItem>(5, 16, deathSymbol));
+
+    map.addItem(make_polymorphic<DeathItem>(26, 23, deathSymbol));
+    map.addItem(make_polymorphic<NullItem>(8, 12, 'n', false));
+    map.addItem(make_polymorphic<NullItem>(8, 11, 'z', false));
+
+    map.addItem(make_polymorphic<DeathItem>(3, 9, deathSymbol));
+    map.addItem(make_polymorphic<Item>(1, 9, ' ', 5, 20));
+    map.addItem(make_polymorphic<Item>(9, 14, ' ', 4, 20));
+
+    map.addItem(make_polymorphic<NullItem>(13, 13, 'h', false));
+    map.addItem(make_polymorphic<NullItem>(13, 12, 'r', false));
+    map.addItem(make_polymorphic<NullItem>(13, 11, 'a', false));
+    map.addItem(make_polymorphic<NullItem>(15, 10, 'n', false));
+    map.addItem(make_polymorphic<NullItem>(15, 9, 'a', false));
+
+    map.addItem(make_polymorphic<Item>(11, 10, ' ', 2, 10));
+    map.addItem(make_polymorphic<Item>(12, 10, ' ', 1, 20));
+    map.addItem(make_polymorphic<Item>(11, 11, ' ', 2, 20));
+
+    map.addItem(make_polymorphic<NullItem>(16, 17, 'r', false));
+    map.addItem(make_polymorphic<NullItem>(21, 16, 'a', false));
+    map.addItem(make_polymorphic<NullItem>(29, 18, 'n', false));
+    map.addItem(make_polymorphic<NullItem>(34, 17, 'd', false));
+    map.addItem(make_polymorphic<NullItem>(20, 14, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(28, 14, 'm', false));
+
+    map.addItem(make_polymorphic<NullItem>(22, 12, 'h', false));
+    map.addItem(make_polymorphic<NullItem>(25, 11, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(28, 12, 'r', false));
+    map.addItem(make_polymorphic<NullItem>(30, 11, 'a', false));
+
+    map.addItem(make_polymorphic<TeleportItem>(41, 11, 37, 11, ' '));
+    map.addItem(make_polymorphic<TeleportItem>(35, 11, 33, 11, ' '));
+
+    map.addItem(make_polymorphic<Item>(36, 10, ' ', 12, 30));
+
+    map.addItem(make_polymorphic<NullItem>(45, 6, 'F', false));
+    map.addItem(make_polymorphic<NullItem>(46, 6, 'i', false));
+    map.addItem(make_polymorphic<NullItem>(47, 6, 'z', false));
+
+    map.addItem(make_polymorphic<NullItem>(18, 7, 'N', false));
+    map.addItem(make_polymorphic<NullItem>(19, 7, 'i', false));
+    map.addItem(make_polymorphic<NullItem>(20, 7, 'k', false));
+    map.addItem(make_polymorphic<NullItem>(28, 7, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(29, 7, 'l', false));
+    map.addItem(make_polymorphic<NullItem>(30, 7, 'a', false));
+
+    map.addItem(make_polymorphic<NullItem>(2, 4, 'a', false));
+    map.addItem(make_polymorphic<NullItem>(3, 4, 'r', false));
+    map.addItem(make_polymorphic<NullItem>(4, 4, 'e', false));
+    map.addItem(make_polymorphic<NullItem>(1, 3, 'g', false));
+    map.addItem(make_polymorphic<NullItem>(2, 3, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(3, 3, 'h', false));
+    map.addItem(make_polymorphic<NullItem>(4, 3, 'o', false));
+    map.addItem(make_polymorphic<NullItem>(5, 3, 'm', false));
+    map.addItem(make_polymorphic<NullItem>(6, 3, 'e', false));
+
+    map.addItem(make_polymorphic<DeathItem>(37, 1, deathSymbol));
+    map.addItem(make_polymorphic<Item>(47, 2, ' ', 10, 34));
+
+    map.addItem(make_polymorphic<Item>(6, 1, ' ', 5, 20));
+    map.addItem(make_polymorphic<Item>(8, 1, ' ', 5, 20));
 
     return map;
 }
